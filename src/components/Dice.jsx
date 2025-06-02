3d286a36d1d302736f02a43c1acce1a9102919ec
21Aditya-patil <adhikpatil2005@gmail.com>
2025-06-02 19:06:59 +0530

    images settings

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -1,9 +1,8 @@
-
 function Dice({current, randomNum}) {
     
   return (      
     <div className='flex flex-col items-center'>
-      <img onClick={randomNum} src={`/public/images/dice_${current}.png`} alt="Dice image" />
+      <img onClick={randomNum} src={`/images/dice_${current}.png`} alt="Dice image" />
       <p className="font-semibold">Click on Dice to roll</p>
     </div>
   )
